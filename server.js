961e055b5f688eb8781b2787ef444ad11199a6a0
rioxu1125 <150398843+rioxu1125@users.noreply.github.com>
2023-12-14 19:00:31 +1100

    Prac 6

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
+
+
+app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/api/cat',router);
+
+
+http.listen(port, ()=>{
+    console.log('express server started');
+});
